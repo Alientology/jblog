1aa872640e34a1c3e5f13016ecb2c49e9fca7b17
Alientology <uxzebcat@sharklasers.com>
2013-07-03 21:08:26 +0200

    And here it is

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ jblog
 =====
 
 Low-fi javascript only blogging for weirdos and deviants
+
+Content is registered in jblog.json and loaded with jquery
---
+++ javascripts/content.js
@@ -0,0 +1,14 @@
+$(function(){
+    var maxDisplay = 10;
+    
+    var pages = new Array();
+  
+    pages = $.getJSON("articles.json",function(){
+	alert("yo");
+	for( var i = 0; pages.length > i && maxDisplay > i; i++ ){
+	    $.get( pages[i].file, function(d) {
+		$("#content").html( $("#content").html()+d);
+	    });
+	}
+    });
+});
